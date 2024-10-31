6c3dfa724ec2ec1087b8d794e3abd052b358b3a6
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2024-10-31 22:51:32 +0800

    Update email.js

--- controllers/email.js
+++ controllers/email.js
@@ -13,32 +13,53 @@ exports.listemail = async (req, res) => {
         limit: parseInt(limit) || 10
     };
 
-    const emaildatas = await Emails.find({
-        $or: [
-            { sender: new mongoose.Types.ObjectId(id) },
-            { receiver: { $elemMatch: { $eq: new mongoose.Types.ObjectId(id) } } },
-            { sendtoall: true }
-        ]
-    })
-    .populate({
-        path: 'sender',
-        select: 'email', // Fields from Users schema
-        populate: { path: 'userDetails', model: 'UserDetails', select: 'firstname lastname' } // Additional details from UserDetails
-    })
-    .populate({
-        path: 'receiver',
-        select: 'email',
-        populate: { path: 'userDetails', model: 'UserDetails', select: 'firstname lastname' }
-    })
-    .skip(pageOptions.page * pageOptions.limit)
-    .limit(pageOptions.limit)
-    .sort({createdAt: -1})
-    .then(data => data)
-    .catch(err => {
-        console.log(`There's a problem with getting the email datas for ${id}. Error: ${err}`)
-
-        return res.status(400).json({message: "bad-request", data: "There's a problem with the server! Please contact customer support."})
-    })
+    const emaildatas = await Emails.aggregate([
+        {
+            $match: {
+                $or: [
+                    { sender: new mongoose.Types.ObjectId(id) },
+                    { receiver: { $elemMatch: { $eq: new mongoose.Types.ObjectId(id) } } },
+                    { sendtoall: true }
+                ]
+            }
+        },
+        {
+            $lookup: {
+                from: 'userdetails', // The collection name of `userDetailsSchema`
+                localField: 'owner',
+                foreignField: 'sender',
+                as: 'senderDetails'
+            }
+        },
+        { $unwind: '$senderDetails' }, // Flatten the `userDetails` array
+        {
+            $lookup: {
+                from: 'userdetails', // The collection name of `userDetailsSchema`
+                localField: 'owner',
+                foreignField: 'receiver',
+                as: 'receiverDetails'
+            }
+        },
+        { $unwind: '$receiverDetails' }, // Flatten the `userDetails` array
+        {
+            $project: {
+                senderfullname: {
+                    $concat: ['$senderDetails.firstname', ' ', '$senderDetails.lastname']
+                },
+                receiverfullname: {
+                    $concat: ['$receiverDetails.firstname', ' ', '$receiverDetails.lastname']
+                },
+                title: 1,
+                content: 1
+            }
+        },
+        {
+            $skip: pageOptions.page * pageOptions.limit,
+        },
+        {
+            $limit: pageOptions.limit,
+        }
+    ])
 
     const totallist = await Emails.countDocuments({
         $or: [
@@ -50,7 +71,7 @@ exports.listemail = async (req, res) => {
 
     const data = {
         totalpage: Math.ceil(totallist / pageOptions.limit),
-        emaillist: emaildatas
+        emaillist: emaildatas[0]
     }
 
     return res.json({message: "success", data: data})
