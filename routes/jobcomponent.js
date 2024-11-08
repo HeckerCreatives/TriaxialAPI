e3c7c9a9a681c5a146a8af6d50f97af6c2dafb47
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2024-11-08 17:43:18 +0800

    Update jobcomponent.js

--- routes/jobcomponent.js
+++ routes/jobcomponent.js
@@ -24,6 +24,7 @@ router
 
     .get("/listjobcomponentemployee", protectemployee, listjobcomponent)
     .post("/editstatushoursemployee", protectemployee, editstatushours)
+    .post("/editjobmanagercomponentsjbmngr", protectemployee, editjobmanagercomponents)
 
     //  #endregion
 
