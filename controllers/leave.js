7480cee491fd9516698faa879586f8c91584a32d
Joshupau <97903490+Joshupau@users.noreply.github.com>
2025-03-08 15:20:58 +0800

    pooosh

--- controllers/leave.js
+++ controllers/leave.js
@@ -256,6 +256,21 @@ exports.requestleave = async (req, res) => {
     })
 
 
+    const getLeaveTypeName = (type) => {
+        const leaveTypes = {
+            '1': 'Annual Leave',
+            '2': 'Sick Leave',
+            '3': "Carer's Leave",
+            '4': 'Bereavement Leave',
+            '5': 'Study Leave',
+            '6': 'Long Services Leave',
+            '7': 'Anniversary Day',
+            '8': 'Paid Parental Leave',
+            '9': 'Time in Lieu',
+            '10': 'Other Leave'
+        };
+        return leaveTypes[type] || 'Unknown Leave Type';
+    };
 
     const sendmailcontent = `
         Good day!
@@ -265,7 +280,7 @@ exports.requestleave = async (req, res) => {
                                         
         Timestamp:                  ${moment().format('YYYY-MM-DD HH:mm:ss')}
         Name:                       ${fullname}
-        Leave Type:                 ${leavetype}
+        Leave Type:                 ${getLeaveTypeName(leavetype)}${leavetype === '10' ? ` (${comments})` : ''}
         Details:                    ${details || 'No details provided'}
         Leave Start Date:           ${leavestart}
         Leave End Date:             ${leaveend}
